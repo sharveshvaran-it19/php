27ce7f15d4e799c7ec65dce4695d1082d7dba628
sharveshvaran-it19 <sharveshvaran.it19@bitsathy.ac.in>
2019-08-21 13:45:46 +0530

    firstcommit

---
+++ display.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head><h1 style="color:blue;">iam bleeding blue</h1></head>
+<body>
+<?php
+$txt="hai";
+echo"$txt";
+echo"<b><marquee>hai my name is<br /> m.sharveshvaran</h<//marque></b>";
+?>
+</body>
+</html>
